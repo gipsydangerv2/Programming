0055a6208fa342e6321c1394614ed3ddca852fda
Ankit Kumar <ankitkr2294@gmail.com>
2016-02-27 12:55:54 +0530

    Update Longest Increasing Subsequence.cpp

--- DP/LongestIncreasingSubsequence.cpp
+++ DP/LongestIncreasingSubsequence.cpp
@@ -3,26 +3,21 @@
 */
 
 #include<iostream>
-#include<string>
+#include<climits>
 using namespace std;
 int LIS(int arr[],int n){
-  int lis[n];
-  for(int i=0;i<n;i++)
-    list[i]=1;
-  for(int i=1;i<n;i++)
+  int table[n];
+  for(int i=0;i<n;i++) arr[i] = 1;
+  for(int i=1;i<n;i++){
     for(int j=0;j<i;j++){
-      if(arr[j]<arr[i] && lis[i]<lis[j]+1)
-        lis[i] = lis[j]+1;
+      if(arr[i]>arr[j] && table[i] < table[j] + 1)
+        table[i] = table[j] + 1;
     }
-  int max=-1;
-  for(int i=0;i<n;i++){
-    if(max<lis[i])
-      max=lis[i];
   }
-  return max;
-}
-int main(){
-  //int arr[];n:length of the array
-  int ans = LIS(arr,n);
-  return 0;
+  int max_val = INT_MIN;
+  for(int i=0;i<n;i++){
+    if(table[i]>max_val)
+     max_val = table[i];
+  }
+  return max_val;
 }
